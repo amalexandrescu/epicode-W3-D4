23512e1425065e97f38fdc9cc3092e7fb7104afe
Alexis <alexandrescu.alexandra.maria@gmail.com>
2022-10-13 22:34:42 +0300

    finalised bingo game

--- app.js
+++ app.js
@@ -33,33 +33,37 @@ let randomNumbersArray = randomiseNumbersUnique();
 console.log(randomNumbersArray);
 
 const h3List = document.querySelectorAll("h3");
-const divList = document.querySelectorAll(".cell");
+let divList = document.querySelectorAll(".cell");
 
 const highlightDrawNumber = function (number) {
-  for (let i = 0, j = 0; i < 76; i++) {
+  for (let i = 0; i < 76; i++) {
     if (number === parseInt(h3List[i].innerText)) {
       divList[i].classList.add("highlight");
     }
   }
 };
 
-let counter = 0;
+// let counter = 0;
 
-const extractBingoNumbers = function () {
-  const actualDraw = randomNumbersArray[counter];
-  highlightDrawNumber(actualDraw);
-  counter++;
-};
+// const extractBingoNumbers = function () {
+//   const actualDraw = randomNumbersArray[counter];
+//   highlightDrawNumber(actualDraw);
+//   highlightDrawNumber2(actualDraw);
+//   counter++;
+// };
 
-const drawNumberButtonNode = document.querySelector("button");
+// const drawNumberButtonNode = document.querySelector("button");
 
-drawNumberButtonNode.addEventListener("click", extractBingoNumbers);
+// drawNumberButtonNode.addEventListener("click", extractBingoNumbers);
 
 let inputNode = document.querySelector("input");
 let inputNumber = parseInt(inputNode.value);
 let x = 1;
 
+let anotherRandomArray = randomiseNumbersUnique();
+
 const generateNewBingoBoard = function (num) {
+  anotherRandomArray = randomiseNumbersUnique();
   inputNode = document.querySelector("input");
 
   inputNumber = parseInt(inputNode.value);
@@ -74,13 +78,16 @@ const generateNewBingoBoard = function (num) {
     nameBoard.innerText = `Board ${x}`;
     const bingoCellNode = document.createElement("div");
     bingoCellNode.className = "cell";
-    const cellNumberNode = document.createElement("h3");
-    cellNumberNode.innerText = i + 1;
+    const cellNumberNode = document.createElement("h4");
+    cellNumberNode.innerText = anotherRandomArray[i];
     divParent.appendChild(bingoCellNode);
     bingoCellNode.appendChild(cellNumberNode);
   }
   x++;
 };
+
+let h4List = document.querySelectorAll("h4");
+let h4ParentList = document.querySelectorAll(".customClass>div");
 
 const input = function () {
   inputNode = document.querySelector("input");
@@ -89,7 +96,34 @@ const input = function () {
   for (let i = 0; i < inputNumber; i++) {
     generateNewBingoBoard(24);
   }
+
+  console.log("h4List: ", (h4List = document.querySelectorAll("h4")));
 };
 
 const generateButton = document.querySelector("#generateBoardsButton");
 generateButton.addEventListener("click", input);
+
+const highlightDrawNumber2 = function (number) {
+  h4List = document.querySelectorAll("h4");
+  h4ParentList = document.querySelectorAll(".customClass>div");
+
+  for (let i = 0; i < h4List.length; i++) {
+    if (number === parseInt(h4List[i].innerText)) {
+      h4ParentList[i].classList.add("highlight");
+    }
+  }
+};
+
+let counter = 0;
+
+const extractBingoNumbers = function () {
+  const actualDraw = randomNumbersArray[counter];
+  highlightDrawNumber(actualDraw);
+  highlightDrawNumber2(actualDraw);
+  counter++;
+};
+
+const drawNumberButtonNode = document.querySelector("button");
+
+drawNumberButtonNode.addEventListener("click", extractBingoNumbers);
+drawNumberButtonNode.addEventListener("click", extractBingoNumbers2);
