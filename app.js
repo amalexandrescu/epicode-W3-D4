7a85347d1f05d4748dbc27cd0fa06fe76f335c46
Alexis <alexandrescu.alexandra.maria@gmail.com>
2022-10-13 17:34:06 +0300

    update app.js with unique randomised numbers

--- app.js
+++ app.js
@@ -7,23 +7,41 @@ const randomiseNumbers = function () {
   return randomNumbersArray;
 };
 
+const randomiseNumbersUnique = function () {
+  const allNumbers = [];
+  for (let i = 1; i <= 76; i++) allNumbers.push(i);
+  const result = [];
+  for (let i = 75; i >= 0; i--) {
+    const randomPosition = Math.floor(Math.random() * (i + 1));
+    result.push(allNumbers[randomPosition]);
+    allNumbers.splice(randomPosition, 1);
+  }
+  return result;
+};
+
 const randomButtonNode = document.querySelector("button");
 
-const createBoardElement = function () {
-  const numArray = randomiseNumbers();
+const createBingoCell = function (cellNumber, isHighlighted) {
   const bingoBoard = document.getElementById("bingo-board");
+  const bingoCellNode = document.createElement("div");
+  bingoCellNode.className = "cell";
+  const cellNumberNode = document.createElement("h3");
+  cellNumberNode.innerText = cellNumber;
+  bingoCellNode.appendChild(cellNumberNode);
+  bingoBoard.appendChild(bingoCellNode);
+  if (isHighlighted) {
+    bingoCellNode.classList.add("highlight");
+  }
+};
+
+const createBoardElement = function () {
+  const bingoBoard = document.getElementById("bingo-board");
+
+  const numArray = randomiseNumbersUnique();
   bingoBoard.innerHTML = "";
 
   for (let i = 0; i < 76; i++) {
-    const bingoCellNode = document.createElement("div");
-    bingoCellNode.className = "cell";
-    const cellNumberNode = document.createElement("h3");
-    cellNumberNode.innerText = numArray[i];
-    bingoCellNode.appendChild(cellNumberNode);
-    bingoBoard.appendChild(bingoCellNode);
-    if (i === numArray[i]) {
-      bingoCellNode.classList.add("highlight");
-    }
+    createBingoCell(numArray[i], numArray[i] === i);
   }
 };
 
