a707df5300727fa6629bb115ee6c9f9c8e3ffe36
Alexis <alexandrescu.alexandra.maria@gmail.com>
2022-10-13 21:24:25 +0300

    bingo boards improved and draw button working

--- app.js
+++ app.js
@@ -1,48 +1,95 @@
-const randomiseNumbers = function () {
-  let randomNumbersArray = [];
+const createBingoCell = function () {
+  const bingoBoard = document.getElementById("bingo-board");
   for (let i = 0; i < 76; i++) {
-    const randomNumber = Math.floor(Math.random() * (76 + 1));
-    randomNumbersArray.push(randomNumber);
+    const bingoCellNode = document.createElement("div");
+    bingoCellNode.className = "cell";
+    const cellNumberNode = document.createElement("h3");
+    cellNumberNode.innerText = i + 1;
+    bingoCellNode.appendChild(cellNumberNode);
+    bingoBoard.appendChild(bingoCellNode);
   }
-  return randomNumbersArray;
 };
+
+createBingoCell();
 
 const randomiseNumbersUnique = function () {
   const allNumbers = [];
-  for (let i = 1; i <= 76; i++) allNumbers.push(i);
-  const result = [];
-  for (let i = 75; i >= 0; i--) {
-    const randomPosition = Math.floor(Math.random() * (i + 1));
-    result.push(allNumbers[randomPosition]);
-    allNumbers.splice(randomPosition, 1);
+  for (let i = 1; i <= 76; i++) {
+    allNumbers.push(i);
   }
+
+  const result = [];
+
+  for (let i = 76; i >= 1; i--) {
+    const randomPosition = Math.ceil(Math.random() * i);
+    result.push(allNumbers[randomPosition - 1]);
+    allNumbers.splice(randomPosition - 1, 1);
+  }
+
   return result;
 };
 
-const randomButtonNode = document.querySelector("button");
+let randomNumbersArray = randomiseNumbersUnique();
+console.log(randomNumbersArray);
 
-const createBingoCell = function (cellNumber, isHighlighted) {
-  const bingoBoard = document.getElementById("bingo-board");
-  const bingoCellNode = document.createElement("div");
-  bingoCellNode.className = "cell";
-  const cellNumberNode = document.createElement("h3");
-  cellNumberNode.innerText = cellNumber;
-  bingoCellNode.appendChild(cellNumberNode);
-  bingoBoard.appendChild(bingoCellNode);
-  if (isHighlighted) {
-    bingoCellNode.classList.add("highlight");
+const h3List = document.querySelectorAll("h3");
+const divList = document.querySelectorAll(".cell");
+
+const highlightDrawNumber = function (number) {
+  for (let i = 0, j = 0; i < 76; i++) {
+    if (number === parseInt(h3List[i].innerText)) {
+      divList[i].classList.add("highlight");
+    }
   }
 };
 
-const createBoardElement = function () {
-  const bingoBoard = document.getElementById("bingo-board");
+let counter = 0;
 
-  const numArray = randomiseNumbersUnique();
-  bingoBoard.innerHTML = "";
+const extractBingoNumbers = function () {
+  const actualDraw = randomNumbersArray[counter];
+  highlightDrawNumber(actualDraw);
+  counter++;
+};
 
-  for (let i = 0; i < 76; i++) {
-    createBingoCell(numArray[i], numArray[i] === i);
+const drawNumberButtonNode = document.querySelector("button");
+
+drawNumberButtonNode.addEventListener("click", extractBingoNumbers);
+
+let inputNode = document.querySelector("input");
+let inputNumber = parseInt(inputNode.value);
+let x = 1;
+
+const generateNewBingoBoard = function (num) {
+  inputNode = document.querySelector("input");
+
+  inputNumber = parseInt(inputNode.value);
+
+  const nameBoard = document.createElement("div");
+  document.body.appendChild(nameBoard);
+  nameBoard.classList.add("board");
+  const divParent = document.createElement("div");
+  document.body.appendChild(divParent);
+  divParent.classList.add("customClass");
+  for (let i = 0; i < num; i++) {
+    nameBoard.innerText = `Board ${x}`;
+    const bingoCellNode = document.createElement("div");
+    bingoCellNode.className = "cell";
+    const cellNumberNode = document.createElement("h3");
+    cellNumberNode.innerText = i + 1;
+    divParent.appendChild(bingoCellNode);
+    bingoCellNode.appendChild(cellNumberNode);
+  }
+  x++;
+};
+
+const input = function () {
+  inputNode = document.querySelector("input");
+  inputNumber = parseInt(inputNode.value);
+
+  for (let i = 0; i < inputNumber; i++) {
+    generateNewBingoBoard(24);
   }
 };
 
-randomButtonNode.addEventListener("click", createBoardElement);
+const generateButton = document.querySelector("#generateBoardsButton");
+generateButton.addEventListener("click", input);
